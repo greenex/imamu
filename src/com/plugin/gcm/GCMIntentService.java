4141a9eed895e6bf7d49382f19b60a1d26d72203
salah <salah.khudairat@gmail.com>
2013-10-20 00:54:58 +0300

    add notification

--- src/com/plugin/gcm/GCMIntentService.java
+++ src/com/plugin/gcm/GCMIntentService.java
@@ -23,141 +23,141 @@ import android.util.Log;
 @SuppressLint("NewApi")
 public class GCMIntentService extends GCMBaseIntentService {
 
-	public static final int NOTIFICATION_ID = 237;
-	private static final String TAG = "GCMIntentService";
-	
-	public GCMIntentService() {
-		super("GCMIntentService");
-	}
+        public static final int NOTIFICATION_ID = 237;
+        private static final String TAG = "GCMIntentService";
+        
+        public GCMIntentService() {
+                super("GCMIntentService");
+        }
 
-	@Override
-	public void onRegistered(Context context, String regId) {
+        @Override
+        public void onRegistered(Context context, String regId) {
 
-		Log.v(TAG, "onRegistered: "+ regId);
+                Log.v(TAG, "onRegistered: "+ regId);
 
-		JSONObject json;
+                JSONObject json;
 
-		try
-		{
-			json = new JSONObject().put("event", "registered");
-			json.put("regid", regId);
+                try
+                {
+                        json = new JSONObject().put("event", "registered");
+                        json.put("regid", regId);
 
-			Log.v(TAG, "onRegistered: " + json.toString());
+                        Log.v(TAG, "onRegistered: " + json.toString());
 
-			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
-			// In this case this is the registration ID
-			PushPlugin.sendJavascript( json );
+                        // Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+                        // In this case this is the registration ID
+                        PushPlugin.sendJavascript( json );
 
-		}
-		catch( JSONException e)
-		{
-			// No message to the user is sent, JSON failed
-			Log.e(TAG, "onRegistered: JSON exception");
-		}
-	}
+                }
+                catch( JSONException e)
+                {
+                        // No message to the user is sent, JSON failed
+                        Log.e(TAG, "onRegistered: JSON exception");
+                }
+        }
 
-	@Override
-	public void onUnregistered(Context context, String regId) {
-		Log.d(TAG, "onUnregistered - regId: " + regId);
-	}
+        @Override
+        public void onUnregistered(Context context, String regId) {
+                Log.d(TAG, "onUnregistered - regId: " + regId);
+        }
 
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		Log.d(TAG, "onMessage - context: " + context);
+        @Override
+        protected void onMessage(Context context, Intent intent) {
+                Log.d(TAG, "onMessage - context: " + context);
 
-		// Extract the payload from the message
-		Bundle extras = intent.getExtras();
-		if (extras != null)
-		{
-			boolean	foreground = this.isInForeground();
+                // Extract the payload from the message
+                Bundle extras = intent.getExtras();
+                if (extras != null)
+                {
+                        boolean        foreground = this.isInForeground();
 
-			extras.putBoolean("foreground", foreground);
+                        extras.putBoolean("foreground", foreground);
 
-			if (foreground)
-				PushPlugin.sendExtras(extras);
-			else
-				createNotification(context, extras);
-		}
-	}
+                        if (foreground)
+                                PushPlugin.sendExtras(extras);
+                        else
+                                createNotification(context, extras);
+                }
+        }
 
-	public void createNotification(Context context, Bundle extras)
-	{
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		String appName = getAppName(this);
+        public void createNotification(Context context, Bundle extras)
+        {
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                String appName = getAppName(this);
 
-		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
-		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		notificationIntent.putExtra("pushBundle", extras);
+                Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+                notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                notificationIntent.putExtra("pushBundle", extras);
 
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);		
+                PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);                
 
-		NotificationCompat.Builder mBuilder = 
-			new NotificationCompat.Builder(context)
-				.setSmallIcon(context.getApplicationInfo().icon)
-				.setWhen(System.currentTimeMillis())
-				.setContentTitle(appName)
-				.setTicker(appName)
-				.setContentIntent(contentIntent);
+                NotificationCompat.Builder mBuilder = 
+                        new NotificationCompat.Builder(context)
+                                .setSmallIcon(context.getApplicationInfo().icon)
+                                .setWhen(System.currentTimeMillis())
+                                .setContentTitle(appName)
+                                .setTicker(appName)
+                                .setContentIntent(contentIntent);
 
-		String message = extras.getString("message");
-		if (message != null) {
-			mBuilder.setContentText(message);
-		} else {
-			mBuilder.setContentText("<missing message content>");
-		}
+                String message = extras.getString("message");
+                if (message != null) {
+                        mBuilder.setContentText(message);
+                } else {
+                        mBuilder.setContentText("<missing message content>");
+                }
 
-		String msgcnt = extras.getString("msgcnt");
-		if (msgcnt != null) {
-			mBuilder.setNumber(Integer.parseInt(msgcnt));
-		}
+                String msgcnt = extras.getString("msgcnt");
+                if (msgcnt != null) {
+                        mBuilder.setNumber(Integer.parseInt(msgcnt));
+                }
 
-		mNotificationManager.notify((String) appName, NOTIFICATION_ID, mBuilder.build());
-		tryPlayRingtone();
-	}
+                mNotificationManager.notify((String) appName, NOTIFICATION_ID, mBuilder.build());
+                tryPlayRingtone();
+        }
 
-	private void tryPlayRingtone() 
-	{
-		try {
-			Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-			Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-			r.play();
-		} 
-		catch (Exception e) {
-			Log.e(TAG, "failed to play notification ringtone");
-		}
-	}
-	
-	public static void cancelNotification(Context context)
-	{
-		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		mNotificationManager.cancel((String)getAppName(context), NOTIFICATION_ID);	
-	}
-	
-	private static String getAppName(Context context)
-	{
-		CharSequence appName = 
-				context
-					.getPackageManager()
-					.getApplicationLabel(context.getApplicationInfo());
-		
-		return (String)appName;
-	}
-	
-	public boolean isInForeground()
-	{
-		ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
-		List<RunningTaskInfo> services = activityManager
-				.getRunningTasks(Integer.MAX_VALUE);
+        private void tryPlayRingtone() 
+        {
+                try {
+                        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                        r.play();
+                } 
+                catch (Exception e) {
+                        Log.e(TAG, "failed to play notification ringtone");
+                }
+        }
+        
+        public static void cancelNotification(Context context)
+        {
+                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.cancel((String)getAppName(context), NOTIFICATION_ID);        
+        }
+        
+        private static String getAppName(Context context)
+        {
+                CharSequence appName = 
+                                context
+                                        .getPackageManager()
+                                        .getApplicationLabel(context.getApplicationInfo());
+                
+                return (String)appName;
+        }
+        
+        public boolean isInForeground()
+        {
+                ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+                List<RunningTaskInfo> services = activityManager
+                                .getRunningTasks(Integer.MAX_VALUE);
 
-		if (services.get(0).topActivity.getPackageName().toString().equalsIgnoreCase(getApplicationContext().getPackageName().toString()))
-			return true;
+                if (services.get(0).topActivity.getPackageName().toString().equalsIgnoreCase(getApplicationContext().getPackageName().toString()))
+                        return true;
 
-		return false;
-	}	
+                return false;
+        }        
 
-	@Override
-	public void onError(Context context, String errorId) {
-		Log.e(TAG, "onError - errorId: " + errorId);
-	}
+        @Override
+        public void onError(Context context, String errorId) {
+                Log.e(TAG, "onError - errorId: " + errorId);
+        }
 
 }
